d2b6239f8eb32734d200de80073f4e4f66e9249b
adil_t <developer5.tomsher@gmail.com>
2026-06-19 14:05:12 +0400

    style: update button hover text color for task edit link

--- resources/views/projects/partials/tasks/modals/detail-content.blade.php
+++ resources/views/projects/partials/tasks/modals/detail-content.blade.php
@@ -46,7 +46,7 @@
 
         <div class="flex items-center gap-3">
             @if ($canOpenTaskDetailPage)
-                <a href="{{ route('tasks.edit', $task) }}" class="inline-flex items-center gap-2 rounded-lg border bg-bgray-100 px-4 py-2 text-sm font-medium text-bgray-700 transition duration-200 hover:text-success-300 dark:border-darkblack-400 dark:bg-darkblack-500 dark:text-success-300 dark:hover:border-success-900/40 dark:hover:bg-darkblack-400 dark:hover:text-success-300">
+                <a href="{{ route('tasks.edit', $task) }}" class="inline-flex items-center gap-2 rounded-lg border bg-bgray-100 px-4 py-2 text-sm font-medium text-bgray-700 transition duration-200 hover:text-success-300 dark:border-darkblack-400 dark:bg-darkblack-500 dark:text-bgray-300 dark:hover:border-success-900/40 dark:hover:bg-darkblack-400 dark:hover:text-bgray-700">
                     <svg xmlns="http://www.w3.org/2000/svg" class="h-4 w-4" viewBox="0 0 20 20" fill="currentColor">
                         <path d="M11 3a1 1 0 100 2h2.586l-6.293 6.293a1 1 0 101.414 1.414L15 6.414V9a1 1 0 102 0V4a1 1 0 00-1-1h-5z" />
                         <path d="M5 5a2 2 0 00-2 2v8a2 2 0 002 2h8a2 2 0 002-2v-3a1 1 0 112 0v3a4 4 0 01-4 4H5a4 4 0 01-4-4V7a4 4 0 014-4h3a1 1 0 110 2H5z" />
