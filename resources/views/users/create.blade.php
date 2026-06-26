d308d889399b33e97eabf9cd53f3b4b097da45a7
adil_t <developer5.tomsher@gmail.com>
2026-06-26 12:19:55 +0400

    feat: add back button to user creation view and adjust header layout

--- resources/views/users/create.blade.php
+++ resources/views/users/create.blade.php
@@ -1,26 +1,28 @@
 @extends('layouts.master')
 
 @section('page-content')
-
-        <!-- write your code here-->
-        <div class="2xl:flex 2xl:space-x-[48px]">
-            <section class="mb-6 2xl:mb-0 2xl:flex-1">
-                <div class="w-full rounded-lg bg-white px-[24px] py-[20px] dark:bg-darkblack-600">
-                    <div class="flex grid-cols-12 flex-col-reverse gap-12 xl:grid 2xl:flex-row">
-                        <div class="col-span-12 w-full">
-                            <h3 class="border-b border-bgray-200 pb-5 text-2xl font-bold text-bgray-900 dark:border-darkblack-400 dark:text-white">
+    <!-- write your code here-->
+    <div class="2xl:flex 2xl:space-x-[48px]">
+        <section class="mb-6 2xl:mb-0 2xl:flex-1">
+            <div class="w-full rounded-lg bg-white px-[24px] py-[20px] dark:bg-darkblack-600">
+                <div class="flex grid-cols-12 flex-col-reverse gap-12 xl:grid 2xl:flex-row">
+                    <div class="col-span-12 w-full">
+                        <div class="border-b border-bgray-200 pb-5 flex flex-row justify-between">
+                            <h3 class="text-2xl font-bold text-bgray-900 dark:border-darkblack-400 dark:text-white">
                                 Create New User
                             </h3>
+                            <x-back-button :url="session('users_return_url', route('users.index'))" />
+                        </div>
 
-                            <div class="mt-8">
-                                @include('users._form', ['user' => null])
-                            </div>
+                        <div class="mt-8">
+                            @include('users._form', ['user' => null])
                         </div>
                     </div>
                 </div>
-            </section>
-        </div>
-        <!-- write your code here-->
+            </div>
+        </section>
+    </div>
+    <!-- write your code here-->
 @endsection
 
 @push('scripts')
