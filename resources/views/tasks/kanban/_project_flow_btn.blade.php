4f3d41bfd1cc9412904e8857dddb10a02aba8155
adil_t <developer5.tomsher@gmail.com>
2026-08-17 13:45:09 +0400

    style: update flow button hover styles and refine agile button text colors

--- resources/views/tasks/kanban/_project_flow_btn.blade.php
+++ resources/views/tasks/kanban/_project_flow_btn.blade.php
@@ -1,6 +1,6 @@
 <div id="flow-switcher" class="inline-flex rounded-lg border sm:ml-auto">
     <div class="relative inline-flex">
-        <button data-flow="agile" class="flow-btn rounded-l-lg px-4 py-2 text-sm font-semibold transition text-gray-700 dark:text-bgray-300 hover:bg-gray-100">
+        <button data-flow="agile" class="flow-btn rounded-l-lg px-4 py-2 text-sm font-semibold transition text-bgray-700 dark:text-bgray-300 dark:hover:text-bgray-900 hover:bg-gray-100">
             Agile
         </button>
         <span class="@if ($agileNewTaskCount <= 0) hidden @endif pointer-events-none absolute -right-1 -top-1 z-10 rounded-full bg-red-500 px-1.5 py-0.5 text-[10px] font-bold leading-none text-white" data-flow-count="agile">{{ $agileNewTaskCount }}</span>
